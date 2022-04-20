47f9890d931c9a3994eddd9c8cc523c2ac00d992
up201907483 <up201907483@edu.fc.up.pt>
2022-04-20 11:56:25 +0100

    merge

--- ClientTCP.java
+++ ClientTCP.java
@@ -1,43 +1,37 @@
 import java.net.*;
 import java.io.*;
- 
-public class ClientTCP
-{
+
+public class ClientTCP{
     // initialize socket and input output streams
     private Socket socket            = null;
     private DataInputStream  input   = null;
     private DataOutputStream out     = null;
- 
+
     // constructor to put ip address and port
-    public ClientTCP(String address, int port)
-    {
+    public ClientTCP(String address, int port){
         // establish a connection
-        try
-        {
+        try{
             socket = new Socket(address, port);
             System.out.println("Connected");
- 
+
             // takes input from terminal
             input  = new DataInputStream(System.in);
- 
+
             // sends output to the socket
             out    = new DataOutputStream(socket.getOutputStream());
         }
-        catch(UnknownHostException u)
-        {
+        catch(UnknownHostException u){
             System.out.println(u);
         }
-        catch(IOException i)
-        {
+        catch(IOException i){
             System.out.println(i);
         }
- 
+
         // string to read message from input
         String line = "";
- 
+
         // keep reading until "Over" is input
-        while (!line.equals("Over"))
-        {
+        while (!line.equals("Over")){
             try
             {
                 line = input.readLine();
@@ -48,22 +42,19 @@ public class ClientTCP
                 System.out.println(i);
             }
         }
- 
+
         // close the connection
-        try
-        {
+        try{
             input.close();
             out.close();
             socket.close();
         }
-        catch(IOException i)
-        {
+        catch(IOException i){
             System.out.println(i);
         }
     }
- 
-    public static void main(String args[])
-    {
+
+    public static void main(String args[]){
         ClientTCP client = new ClientTCP("127.0.0.1", 8000);
     }
 }

28f3fec3ac252af70d963f4d11ef1a7e7ee2c4d7
JJJJJJJJJJJJJJJl <costa.joaom@hotmail.com>
2022-04-21 21:50:52 +0100

    file removed - useless

--- TestMulticast.java
+++
@@ -1,9 +0,0 @@
-import java.io.*;
-
-public class TestMulticast{
-    public static void main(String args[]){
-        MulticastServer s1 = new MulticastServer();
-        System.out.println(s1);
-        MulticastClient c1 = new MulticastClient();
-    }
-}

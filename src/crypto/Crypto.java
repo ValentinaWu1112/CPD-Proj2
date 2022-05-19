6f5f139c7e35bc547a1ca3498fb76aac2e03408b
JJJJJJJJJJJJJJJl <costa.joaom@hotmail.com>
2022-05-19 11:04:02 +0100

    Crypo class - encodeValue method now returns hexadecimal string

--- src/crypto/Crypto.java
+++ src/crypto/Crypto.java
@@ -2,6 +2,7 @@ package crypto;
 
 import java.security.MessageDigest;
 import java.nio.charset.StandardCharsets;
+import java.math.BigInteger;
 
 public final class Crypto {
 
@@ -11,7 +12,10 @@ public final class Crypto {
         try {
             MessageDigest digest = MessageDigest.getInstance("SHA-256");
             byte[] hash = digest.digest(value.getBytes(StandardCharsets.UTF_8));
-            return new String(hash, StandardCharsets.US_ASCII);
+            BigInteger number = new BigInteger(1, hash);
+            StringBuilder hex_string = new StringBuilder(number.toString(16));
+            while (hex_string.length() < 64) hex_string.insert(0, '0');
+            return hex_string.toString();
         } catch (Exception e) {
             e.printStackTrace();
         }
